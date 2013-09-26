a36ff37fc499f3026cc88d4aa3fc2ec8762d35c2
mclean <mclean@mapserver-local.mclean.local>
2013-09-26 07:50:14 -0400

    Updated the Authentication Stragities

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
 var passport = require("passport");
 var fs = require("fs");
+
+var redis = require("redis");
+var client = redis.createClient();
+
+//TODO for now we are going to use REDIS
+/*
+client.hmset('clint.fleetwood@gmail.com', {
+	username:"cefleet",
+	email:"clint.fleetwood@gmail.com",
+	types:'{"google":{"identifier":"AItOawl6Ly-Ux5CDpNgeDkT0j35P76XJOYRVkmY", "active":"true"}, "local":{"active":"true"}}',
+	apps:'{"woi":{"type":"admin"}}'
+});
+*/
+/*
+client.hmset('clint.fleetwood@gmail.com:info', {
+	types:'{"google":{"identifier":"AItOawl6Ly-Ux5CDpNgeDkT0j35P76XJOYRVkmY", "active":"true"}}',
+	apps:'{"woi":{"type":"admin"}}'
+});
+
+client.hget("clint.fleetwood@gmail.com", "type", function(err,types){
+	types = types.split(',');
+	if(types.indexOf('google') > -1){
+		console.log('We have google action over here');
+		client.hget("clint.fleetwood@gmail.com:info","types", function(err,types){
+			console.log(JSON.parse(types).google.identifier);
+		});
+	}
+});
+*/
+/*
 var Users = {
 	cefleet:{username:'cefleet',password:'password', id:'1'},
 	user:{username:'user', password:'password2', id:'2'},
 	clint2: {username:'cefleet2',password:'3rg34f4D3423f',id:'3', type:['google'],identifier:'AItOawl6Ly-Ux5CDpNgeDkT0j35P76XJOYRVkmY'}
 }
+*/
+
 var Auth = function Auth(app,express){
 	this.authenticate = function(req,res,next){
 		var type = req.params.type;
 		var action = req.params.action;
 		fs.exists(__dirname+'/lib/strategies/'+type+'/'+type+'.js', function(exists){
 			if(exists){
-				require('./lib/strategies/'+type+'/'+type).strategy(passport, Users);
+				require('./lib/strategies/'+type+'/'+type).strategy(passport,client);
 				fs.exists(__dirname+'/lib/strategies/'+type+'/'+action+'.js', function(_exists){
 					if(_exists){
 						require('./lib/strategies/'+type+'/'+action)(passport,req,res,next);
@@ -26,18 +58,18 @@ var Auth = function Auth(app,express){
     this.app.use(passport.initialize());
 	this.app.use(passport.session());	
 	
-	//passport stragety for google
 	passport.serializeUser(function(user, done) {
-		console.log(user);
-	    done(null, user.id);
+	    done(null, user.email);
 	});
 
-	passport.deserializeUser(function(id, done) {
-	    for(var user in Users){
-	    	if(Users[user].id == id){
-	    		done(null,user);
-	    	}
-	    }
+	passport.deserializeUser(function(email, done) {
+		client.hgetall(email, function(err,user){			
+			if(err || user == null){
+				done(null, false,{message:'Error Verifying User'});
+			} else {	
+				done(null,user);
+			}
+		});	    
 	});
 	
 	this.app.all('/auth/:type/:action',function(req,res,next){
